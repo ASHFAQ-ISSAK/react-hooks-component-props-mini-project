66b70ba6a82bce13ae06e13a8124cb9b9f1c236c
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-04-20 18:37:45 +0300

    Added compoments to show data from blog on page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
 import blogData from "../data/blog";
-
-console.log(blogData);
+import blogLogo from "../assets/logo.svg";
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={blogData.name} />
+      <ArticleList posts={blogData.posts} />
+      <About image={blogLogo} about={blogData.about} />
     </div>
   );
 }
